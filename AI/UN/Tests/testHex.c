ec573681d89f7fac6b1d2ebe34ff20c88d7982a0
Sprax Lines <sprax@comcast.net>
2016-06-09 22:17:23 -0400

    testHex

--- AI/UN/Tests/testHex.c
+++ AI/UN/Tests/testHex.c
@@ -2,17 +2,15 @@
 #include <stdio.h>
 #include <limits.h>
 
-void main(int argc, char *argv, char **envp) { 
+int main(int argc, char **argv, char **envp) { 
 
-printf(" INT_MAX is %lu\n",  INT_MAX);
-printf("UINT_MAX is %lu\n", UINT_MAX);
-printf("0x00000000 is %lu\n", 0x00000000);
-printf("0x0000000F is %lu\n", 0x0000000F);
-printf("0x000000F0 is %lu\n", 0x000000F0);
-printf("0x000000FF is %lu\n", 0x000000FF);
-printf("0x0F000000 is %lu\n", 0x0F000000);
-printf("0xF0000000 is %lu\n", 0xF0000000);
-
+printf(" INT_MAX is %d\n",  INT_MAX);
+printf("UINT_MAX is %u\n", UINT_MAX);
+printf("0x00000000 is %u\n", 0x00000000);
+printf("0x0000000F is %u\n", 0x0000000F);
+printf("0x000000F0 is %u\n", 0x000000F0);
+printf("0x000000FF is %u\n", 0x000000FF);
+printf("0x0F000000 is %u\n", 0x0F000000);
+printf("0xF0000000 is %u\n", 0xF0000000);
 }
-
 
